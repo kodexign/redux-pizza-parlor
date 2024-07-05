9ba6b270e5a5a318253ffc095963c9167ba57a6a
Kalia Xiong <kaliaxyooj@gmail.com>
2024-07-04 21:13:36 -0500

    added customerinfo

---
+++ src/components/Customerinfo/Customerinfo.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+function CustomerInfo() {
+    return{
+
+    }
+}
+export default CustomerInfo;
